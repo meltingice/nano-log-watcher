aa40e1d26429305f8c44f670b30f00c7a6c4203f
Ryan LeFevre <meltingice8917@gmail.com>
2018-04-16 11:10:39 -0400

    Move log streamer to own file

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-var os = require("os");
-var fs = require("fs");
-const ts = require("tail-stream");
+const os = require("os");
 const Gelf = require("gelf");
 
-const HOSTNAME = os.hostname();
-const RAIBLOCKS_DIR = "/home/ryanlefevre/RaiBlocks";
-const LOG_DIR = `${RAIBLOCKS_DIR}/log`;
-
-var stream = null;
+const config = {
+  HOSTNAME: os.hostname(),
+  RAIBLOCKS_DIR: "/home/ryanlefevre/RaiBlocks",
+  LOG_DIR: `${RAIBLOCKS_DIR}/log`,
+  RPC_ADDRESS: process.env.RPC_ADDRESS || "127.0.0.1:7076"
+};
 
 const gelf = new Gelf({
   graylogPort: process.env.GRAYLOG_PORT || 12201,
@@ -15,49 +14,6 @@ const gelf = new Gelf({
   connection: "lan"
 });
 
-function watchForNewLogFile() {
-  fs.watch(LOG_DIR, { encoding: "utf8" }, (eventType, filename) => {
-    if (filename && eventType === "rename" && /^log_/.test(filename)) {
-      console.log("New log file detected!");
-      streamLogFile(filename);
-    }
-  });
-}
+const Logs = require("./lib/logs")(gelf, config);
 
-function streamLogFile(filename, isRetry) {
-  if (stream) stream.destroy();
-
-  console.log(`Beginning stream of ${filename}`);
-
-  stream = ts.createReadStream(`${LOG_DIR}/${filename}`, {
-    onMove: "end",
-    detectTruncate: false,
-    endOnError: true,
-    waitForCreate: true
-  });
-
-  stream.on("data", data => {
-    data
-      .toString()
-      .split("\n")
-      .forEach(line => {
-        if (!line) return;
-
-        gelf.emit("gelf.log", {
-          version: "1.1",
-          host: HOSTNAME,
-          short_message: line,
-          timestamp: Math.floor(new Date().getTime() / 1000),
-          level: 7
-        });
-      });
-  });
-
-  stream.on("error", error => {
-    console.log("ERROR: ", error);
-  });
-}
-
-const files = fs.readdirSync(LOG_DIR);
-streamLogFile(files[files.length - 1]);
-watchForNewLogFile();
+Logs.start();
